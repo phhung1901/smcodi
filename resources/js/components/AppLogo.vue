15bb41edb32364c59414d1534b5ca01c8bf29ae6
phhung1901 <hungph@axalize.vn>
2025-09-28 01:57:24 +0700

    hotfix

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,12 +3,8 @@ import SMCodiLogo from '@/components/SMCodiLogo.vue';
 </script>
 
 <template>
-    <div class="flex items-center justify-center">
+    <div class="flex items-center gap-2">
         <SMCodiLogo variant="auto" class="h-8" />
-    </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >SMCodi</span
-        >
+        <span class="text-sm font-semibold leading-none">SMCodi</span>
     </div>
 </template>
